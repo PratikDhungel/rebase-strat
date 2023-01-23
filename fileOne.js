e15bb1065452a1d25cf8296ff80f7ce0d76ffd4c
Pratik Dhungel <pratikdhungel7.0@gmail.com>
2023-01-23 18:23:03 +0545

    Feature branch 1

--- fileOne.js
+++ fileOne.js
@@ -7,11 +7,19 @@ const initialFunction = () => {
 const branchOne = () => {
   console.log('This is the first branch one commit');
 
+<<<<<<< HEAD
   console.log('Fix branch made an update in branchOne');
+=======
+  console.log('Feature branch is adding a new feature in branchOneFunction');
+>>>>>>> Feature branch 1
 };
 
 const branchTwoFunction = () => {
   console.log('Commit in branch two on same line as branch one');
 
+<<<<<<< HEAD
   console.log('Fix branch made an update in branchTwo with new commit');
+=======
+  console.log('Feature branch is adding a new feature in branchTwoFunction');
+>>>>>>> Feature branch 1
 };
